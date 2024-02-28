c072c4cd7a589fda1dd68c38f86fd9103f793b9c
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-02-28 11:30:06 -0500

    Added Navbar and installed TailwindCSS library

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="bg-orange-200 fixed w-full z-20 top-0 start-0 border-b dark:border-gray-300 py-5 shadow-lg">
+            <div className="max-w-screen-xl flex items-center justify-between mx-auto p-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                <a href="http://google.com" className='mx-6'>
+                    <img src="/VillageLogo.png" alt="Logo with heart in hand" width="80"></img>
+                    <span className='self-center my-2 text-sm font-semibold'>It takes a Village...</span>
+                </a>
+
+                <div className='flex items-center space-x-3 md:space-x-0 rtl:space-x-reverse'>
+                    <button data-collapse-toggle="navbar-hamburger" type="button" onClick={toggleMenu} className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded={isMenuOpen}>
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div className={`absolute right-0 mt-2 ${isMenuOpen ? ' ' : 'hidden'}`} id="navbar-hamburger">
+                <ul className='flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600' aria-current="page"> Home</a>
+                    </li>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-gray-900 rounded hover:bg-yellow-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' aria-current="page"> Register</a>
+                    </li>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-gray-900 rounded hover:bg-yellow-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' aria-current="page"> Sign In</a>
+                    </li>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-gray-900 rounded hover:bg-yellow-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' aria-current="page"> Profile</a>
+                    </li>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-gray-900 rounded hover:bg-yellow-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' aria-current="page"> Emergency Contacts</a>
+                    </li>
+                    <li>
+                        <a href="#" className='block py-2 px-3 text-gray-900 rounded hover:bg-yellow-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' aria-current="page"> Medicine Cabinet</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav >
+    );
+};
+
+export default Navbar;
