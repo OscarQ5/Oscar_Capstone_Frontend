3adbc3784fe7d9b19406958155ca05aede6c0dce
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-03-02 13:17:21 -0500

    Adjusted the Routes in app.jsx and removed BrowserRouter as Router in the imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { BrowserRouter as Routes, Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx'
 import './App.css'
 
@@ -10,17 +10,14 @@ function App() {
     <>
       <Navbar />
       <div>
-        <Router>
-          <Routes>
-            <Route path='/' element={<LandingPage />} />
-            <Route path='/users/sign-up' element={< Signup />} />
-            {/* <Route path='/users/login' element={< />} /> */}
-            {/* <Route path='/' element={< />} / > */}
-            {/* <Route path="users/:user_id/contacts/sign-up" element={<EmergencyContactsPage />} />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/users/sign-up' element={< Signup />} />
+          {/* <Route path='/users/login' element={< />} /> */}
+          {/* <Route path='/' element={< />} / > */}
+          {/* <Route path="users/:user_id/contacts/sign-up" element={<EmergencyContactsPage />} />
             <Route path="users/:user_id/contacts/sign-up2" element={<MedicalHistoryPage />} /> */}
-          </Routes>
-        </Router>
-
+        </Routes>
       </div>
     </>
   )
