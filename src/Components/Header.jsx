ce29aaa0652bb1441b7f3e6fe45dba676622062a
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-03-03 20:14:31 -0500

    Added Landing.jsx and Header.jsx with respective .css files. Changed a href tags with Link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import '../Styles/Header.css';
 
 const Header = () => {
+
+    const API = import.meta.env.VITE_API_URL;
+
     return (
-        <div className='bg-gray-400 fixed w-full z-20 top-0 start-0 border-b dark:border-gray-300 py-6 shadow-lg'>
-            <div className=''>
-                <a href="/Signin">
-                    <img src="./login.png" alt="Sign In icon" width="75" className='relative bottom-4 right-1 hover:bg-red-800'></img>
-                </a>
-            </div>
-            <div className="">
-                <a href="/Signup">
-                    <img src="./sign-up.png" alt="Sign up icon" width="65" className='absolute right-5 top-5 py-2 px-2 hover:bg-red-800'></img>
-                </a>
-            </div>
+
+        <div className='header-bg'>
+            <Link to={`${API}/users/login`}>
+                <img src="./login.png" alt="Sign In icon" className='login-style' />
+            </Link>
+
+            <Link to={`${API}/users/Sign-up`} >
+                <img src="./signup.png" alt="Sign up icon" className='signup-style' />
+            </Link>
+
         </div>
+
 
     );
 };
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header.jsx';
+import Landing from '../Components/Landing.jsx';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Header />
+            <Landing />
+        </div>
+    );
+};
+
+export default LandingPage;
