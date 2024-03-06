c42c254f66a055b4cd89d9c4f9e3ca9303f873f3
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-03-06 12:28:03 -0500

    Added Logout that navigates to Landing page

---
+++ src/Styles/MedicineCabineto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MedicineCabineto = () => {
+    return (
+        <div className='cabinet-background'>
+
+
+        </div>
+    );
+};
+
+export default MedicineCabineto;
